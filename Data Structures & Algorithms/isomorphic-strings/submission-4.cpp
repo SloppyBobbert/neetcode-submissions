91c5cf9a1bb98be8706aedf52dcba5e0c1ce8406
SloppyBobbert <19805162+SloppyBobbert@users.noreply.github.com>
2026-09-20 01:43:12 -0700

    Add: isomorphic-strings - submission-4

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        vector<int> sToT(256, -1);
+        vector<int> tToS(256, -1);
+
+        for (int i = 0; i < s.size(); ++i) {
+            unsigned char a = s[i];
+            unsigned char b = t[i];
+
+            if (sToT[a] == -1 && tToS[b] == -1) {
+                sToT[a] = b;
+                tToS[b] = a;
+            } 
+            else if (sToT[a] != b || tToS[b] != a) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
